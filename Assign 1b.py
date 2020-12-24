3d056fe2044c2d13c61c6b7437b225a1713a3f12
SUBATHRAMURUGAN <51949909+SUBATHRAMURUGAN@users.noreply.github.com>
2020-12-24 18:32:09 +0530

    Create Assign 1b.py

---
+++ Assign 1b.py
@@ -0,0 +1,26 @@
+# What does + operator doing in both case ?
+
+# 42+2 ---- does addition of integer
+
+a = 42
+b = 2
+c = a + b
+print(c)
+
+"""
+O/P :
+44
+"""
+
+-------------------------------------------------------------------------------------------------------------
+# "42" + "2" ---- does concatenation of string
+
+a = "42"
+b = "2"
+c = a + b
+print(c)
+
+"""
+O/P  :
+422
+"""
