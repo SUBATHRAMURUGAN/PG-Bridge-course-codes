fcb62f45ee3883b69070722f3f81ecbe30436a8f
SUBATHRAMURUGAN <51949909+SUBATHRAMURUGAN@users.noreply.github.com>
2020-12-24 18:27:59 +0530

    Create Assign 1a.py

---
+++ Assign 1a.py
@@ -0,0 +1,27 @@
+# To get a number from user in x and print values of x,x2,x3,x4?
+
+x = int(input("Enter any number = "))
+print(x)
+y = pow(x, 2)
+print(y)
+z = pow(x, 3)
+print(z)
+a = pow(x, 4)
+print(a)
+
+"""
+x = int(input("Enter any number = "))
+print(x)
+y = x**2
+print(y)
+z = x**3
+print(z)
+a = x**4
+print(a)
+
+o/p :
+Enter any number = 2
+4
+8
+16
+"""
